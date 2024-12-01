9973c0589265ab9e4d582c300b662ee6bd807117
Het-Kotadiya <hetkotadiya0303@gmail.com>
2024-12-01 14:53:01 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 // Middleware
 app.use(cors({
     origin: [
-        'https://file-shield.vercel.app/', // Your Vercel frontend URL
+        'https://file-shield.vercel.app', // Your Vercel frontend URL
         'http://localhost:8080' // Local development URL
       ],
       methods: ['GET', 'POST'],
